610f4c6f2c9d66e72b630f4014ef9c1b9d94a36c
Anderson Rocha <anderson_olr@outlook.com>
2020-02-19 23:00:38 -0300

    Initial implementation.

---
+++ FluentDecoration/ObjectExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace FluentDecoration
+{
+    public static class ObjectExtensions
+    {
+        public static bool IsNull<T>(this T obj) where T : class => obj is null;
+
+        public static bool IsNotNull<T>(this T obj) where T : class => !IsNull(obj);
+
+        public static bool ElseThrow(this bool success, System.Exception ex)
+        {
+            if (!success)
+                throw ex;
+
+            return success;
+        }
+
+        public static bool ElseThrow(this bool success, string msg)
+        {
+            if (!success)
+                throw new System.Exception(msg);
+
+            return success;
+        }
+    }
+}
