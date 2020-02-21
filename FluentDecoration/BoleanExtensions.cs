0b930b4f7dd7266a05aa9bb00ff995953773d569
Anderson Rocha <anderson_olr@outlook.com>
2020-02-21 20:48:53 -0300

    Fluent boolean expressions are added.

---
+++ FluentDecoration/BoleanExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FluentDecoration
+{
+    public static class BoleanExtensions
+    {
+        public static bool And(this bool b1, bool b2)
+        {
+            return b1 && b2;
+        }
+    }
+}
