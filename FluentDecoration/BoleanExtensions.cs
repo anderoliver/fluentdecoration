4ee9eaf4b1048a8149196dc6beccf942281a1bae
Anderson Rocha <anderson_olr@outlook.com>
2020-02-21 21:41:35 -0300

    Code cleaning

--- FluentDecoration/BoleanExtensions.cs
+++ FluentDecoration/BoleanExtensions.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace FluentDecoration
+﻿namespace FluentDecoration
 {
     public static class BoleanExtensions
     {
-        public static bool And(this bool b1, bool b2) => b1 && b2;
-        public static bool Or(this bool b1, bool b2) => b1 || b2;
+        public static bool And(this bool b1, bool b2)
+        {
+            return b1 && b2;
+        }
+
+        public static bool Or(this bool b1, bool b2)
+        {
+            return b1 || b2;
+        }
     }
 }
--- FluentDecoration/ObjectExtensions.cs
+++ FluentDecoration/ObjectExtensions.cs
@@ -2,14 +2,22 @@
 {
     public static class ObjectExtensions
     {
-        public static bool IsNull<T>(this T obj) where T : class => obj is null;
+        public static bool IsNull<T>(this T obj) where T : class
+        {
+            return obj is null;
+        }
 
-        public static bool IsNotNull<T>(this T obj) where T : class => !IsNull(obj);
+        public static bool IsNotNull<T>(this T obj) where T : class
+        {
+            return !IsNull(obj);
+        }
 
         public static bool ElseThrow(this bool success, System.Exception ex)
         {
             if (!success)
+            {
                 throw ex;
+            }
 
             return success;
         }
@@ -17,7 +25,9 @@
         public static bool ElseThrow(this bool success, string msg)
         {
             if (!success)
+            {
                 throw new System.Exception(msg);
+            }
 
             return success;
         }
