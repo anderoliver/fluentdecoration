1aa5b3bff23f7c432c5eb97243b7875a7594587b
Anderson Rocha <anderson_olr@outlook.com>
2020-02-21 21:39:06 -0300

    Included Or expression

--- FluentDecoration/BoleanExtensions.cs
+++ FluentDecoration/BoleanExtensions.cs
@@ -6,9 +6,7 @@ namespace FluentDecoration
 {
     public static class BoleanExtensions
     {
-        public static bool And(this bool b1, bool b2)
-        {
-            return b1 && b2;
-        }
+        public static bool And(this bool b1, bool b2) => b1 && b2;
+        public static bool Or(this bool b1, bool b2) => b1 || b2;
     }
 }
