e252d83ec82b518354e0736c75dab15424b82bb3
Anderson Rocha <anderson_olr@outlook.com>
2020-02-27 22:15:19 -0300

    Version 1.0.2 documented.

--- FluentDecoration/BoleanExtensions.cs
+++ FluentDecoration/BoleanExtensions.cs
@@ -1,12 +1,29 @@
 ﻿namespace FluentDecoration
 {
+    /// <summary>
+    /// Provides extension methods for common operations and validations for booleans.
+    /// </summary>
     public static class BoleanExtensions
     {
+        /// <summary>
+        /// <para>Verifies if <paramref name="b1"/> and <paramref name="b2"/> are true.</para>
+        /// <para>Returns true whether <paramref name="b1"/> and <paramref name="b2"/> are true, otherwise false.</para>
+        /// </summary>
+        /// <param name="b1">First boolean.</param>
+        /// <param name="b2">Second boolean.</param>
+        /// <returns>Returns true whether <paramref name="b1"/> and <paramref name="b2"/> are true, otherwise false.</returns>
         public static bool And(this bool b1, bool b2)
         {
             return b1 && b2;
         }
 
+        /// <summary>
+        /// <para>Verifies if <paramref name="b1"/> or <paramref name="b2"/> are true.</para>
+        /// <para>Returns true whether <paramref name="b1"/> or <paramref name="b2"/> are true, otherwise false.</para>
+        /// </summary>
+        /// <param name="b1">First boolean.</param>
+        /// <param name="b2">Second boolean.</param>
+        /// <returns>Returns true whether <paramref name="b1"/> or <paramref name="b2"/> are true, otherwise false.</returns>
         public static bool Or(this bool b1, bool b2)
         {
             return b1 || b2;
--- FluentDecoration/ObjectExtensions.cs
+++ FluentDecoration/ObjectExtensions.cs
@@ -1,17 +1,41 @@
 ﻿namespace FluentDecoration
 {
+    /// <summary>
+    /// Provides extension methods for common operations and validations for all of objects.
+    /// </summary>
     public static class ObjectExtensions
     {
+        /// <summary>
+        /// <para>Verifies if an object is null.</para>
+        /// <para>Returns true whether <paramref name="obj"/> is null, otherwise false.</para>
+        /// </summary>
+        /// <typeparam name="T">The type of the object to verify.</typeparam>
+        /// <param name="obj">The object instance to verify.</param>
+        /// <returns>Returns true whether <paramref name="obj"/> is null, otherwise false.</returns>
         public static bool IsNull<T>(this T obj) where T : class
         {
             return obj is null;
         }
 
+        /// <summary>
+        /// <para>Verifies if an object is not null.</para>
+        /// <para>Returns true whether <paramref name="obj"/> is not null, otherwise false.</para>
+        /// </summary>
+        /// <typeparam name="T">The type of the object to verify.</typeparam>
+        /// <param name="obj">The object instance to verify.</param>
+        /// <returns>Returns true whether <paramref name="obj"/> is not null, otherwise false.</returns>
         public static bool IsNotNull<T>(this T obj) where T : class
         {
             return !IsNull(obj);
         }
 
+        /// <summary>
+        /// <para>Throws an exception when check the <paramref name="success"/> value.</para>
+        /// <para>Returns the <paramref name="success"/> value whether true, otherwise throws the exception <paramref name="ex"/>.</para>
+        /// </summary>
+        /// <param name="success">The conditional value to throw the exception.</param>
+        /// <param name="ex">The exception to throw.</param>
+        /// <returns>Returns the <paramref name="success"/> value whether true, otherwise throws the exception <paramref name="ex"/>.</returns>
         public static bool ElseThrow(this bool success, System.Exception ex)
         {
             if (!success)
@@ -22,6 +46,13 @@
             return success;
         }
 
+        /// <summary>
+        /// <para>Throws an exception when check the <paramref name="success"/> value.</para>
+        /// <para>Returns the <paramref name="success"/> value whether true, otherwise throws an <see cref="System.Exception"/> with <paramref name="msg"/> as message.</para>
+        /// </summary>
+        /// <param name="success">The conditional value to throw the exception.</param>
+        /// <param name="msg">The exception message.</param>
+        /// <returns>Returns the <paramref name="success"/> value whether true, otherwise throws an <see cref="System.Exception"/> with <paramref name="msg"/> as message.</returns>
         public static bool ElseThrow(this bool success, string msg)
         {
             if (!success)
